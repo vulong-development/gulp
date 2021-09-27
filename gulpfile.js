07d1e6affcd43f62d943cd62e6d612ddb0da5c0a
Ramon <kudgar@gmail.com>
2021-09-27 20:36:40 +0300

    create gulp

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const {src, dest, series, watch} = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const csso = require('gulp-csso')
+const include = require('gulp-file-include')
+const htmlmin = require('gulp-htmlmin')
+const del = require('del')
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+const sync = require('browser-sync').create()
+
+function html() {
+  return src('src/**.html')
+    .pipe(include({
+      prefix: '@@'
+    }))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(dest('dist'))  
+}
+
+function scss() {
+  return src('src/scss/**scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 2 versions']
+    }))
+    .pipe(csso())
+    .pipe(concat('style.css'))
+    .pipe(dest('dist'))
+}
+
+function clear() {
+  return del('dist')
+}
+
+function serve() {
+  sync.init({
+    server: './dist'
+  })
+
+  watch('src/**.html', series(html)).on('change', sync.reload)
+  watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
+}
+
+exports.build = series(clear, scss, html)          //Для продакшена
+exports.serve = series(clear, scss, html, serve)   //Для разработки
+exports.clear = clear
