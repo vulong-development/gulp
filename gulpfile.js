c013e560d65b256dc7bb9b5d59aef4f6012e86e8
Ramon <kudgar@gmail.com>
2021-09-27 21:02:59 +0300

    rebuild gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const {src, dest, series, watch} = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-const csso = require('gulp-csso')
+const sass = require('gulp-sass')(require('sass'));  //Sass компилятор
+const csso = require('gulp-csso');  //Минификация css
 const include = require('gulp-file-include')
 const htmlmin = require('gulp-htmlmin')
 const del = require('del')
-const concat = require('gulp-concat')
-const autoprefixer = require('gulp-autoprefixer')
+const concat = require('gulp-concat');   // Объединение набора файлов в один
+const autoprefixer = require('gulp-autoprefixer');  // Добавление вендорных префиксов
 const sync = require('browser-sync').create()
 
 function html() {
