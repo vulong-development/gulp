f713d5e327c304e673ea5e1bdf3aac2c0ac4d28b
Versandi <kudgar@yandex.ru>
2022-05-31 14:22:15 +0300

    Добавлен browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-const {src, dest, series, watch} = require('gulp')
-const sass = require('gulp-sass')(require('sass'));  //Sass компилятор
-const csso = require('gulp-csso');  //Минификация css
+const { src, dest, series, watch } = require('gulp')
+const sass = require('gulp-sass')(require('sass'));   //Sass компилятор
+const csso = require('gulp-csso');                    //Минификация css
 const include = require('gulp-file-include')
 const htmlmin = require('gulp-htmlmin')
 const del = require('del')
-const concat = require('gulp-concat');   // Объединение набора файлов в один
-const autoprefixer = require('gulp-autoprefixer');  // Добавление вендорных префиксов
+const concat = require('gulp-concat');                // Объединение набора файлов в один
+const autoprefixer = require('gulp-autoprefixer');   // Добавление вендорных префиксов
 const sync = require('browser-sync').create()
 
 function html() {
@@ -16,7 +16,7 @@ function html() {
     .pipe(htmlmin({
       collapseWhitespace: true
     }))
-    .pipe(dest('dist'))  
+    .pipe(dest('dist'))
 }
 
 function scss() {
